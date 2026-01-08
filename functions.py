18d8c93f2a53038785cccfb01f622ccd45808590
ryand <ryandannydavis@gmail.com>
2026-01-08 13:29:40 -0500

    Implemented the Add button

--- functions.py
+++ functions.py
@@ -1,9 +1,12 @@
-x="me"
-y="you"
-print(x,y)
+FILEPATH = "todos_item.txt"
 
-print(x+y)
 
-file = open("test.txt",'w')
-file.writelines(["one","two"])
-file.close()
+def get_todos(filepath=FILEPATH):
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+        return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, "w") as file:
+        file.writelines(todos_arg)
--- gui.py
+++ gui.py
@@ -2,9 +2,27 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My To-Do App", layout=[[label,input_box, add_button]])
-window.read()
+window = sg.Window("My To-Do App",
+                   layout=[[label, input_box, add_button]],
+                   font=("Helvetica", 20))
+while True:
+    event, value = window.read()
+    print("Event ", event)
+    print("Value ", value)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = value["todo"] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case "Edit":
+            pass
+        case "Complete":
+            pass
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
